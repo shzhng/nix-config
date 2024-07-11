20cbcd86b114475599385a3f7d1b6d97f3c53174
Shuo Zheng <github.com@shuo.dev>
2024-07-10 21:34:08 -0400

    set up a modules structure

--- flake.nix
+++ flake.nix
@@ -14,69 +14,22 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [
-          vim
-          bat
-          fd
-          fzf
-          git
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      environment.shells = with pkgs; [ zsh fish ];
-
-      users.users = {
-        shuo = {
-          home = "/Users/shuo";
-          shell = pkgs.zsh;
-        };
-        notshuo = {
-          home = "/Users/notshuo";
-          shell = pkgs.zsh;
-        };
-      };
-    };
-  in
-  {
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }: {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#shuos-macbook-air
+    # $ darwin-rebuild build --flake .#Shuos-Macbook-Air
     darwinConfigurations."Shuos-Macbook-Air" = nix-darwin.lib.darwinSystem {
       modules = [
-        configuration
-        home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.shuo = import ./home.nix;
+        ./modules/darwin
+        home-manager.darwinModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.shuo = import ./home.nix;
+          };
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
       ];
     };
 
