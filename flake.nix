d6eca266044f0652ca763273ab227e8eaff3e1b8
Shuo Zheng <github.com@shuo.dev>
2024-07-10 21:55:29 -0400

    added a gitignore

--- flake.nix
+++ flake.nix
@@ -7,11 +7,15 @@
     # it'll impact your entire system.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }: {
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,7 +3,7 @@
   users.users = {
     shuo = {
       home = "/Users/shuo";
-      shell = pkgs.zsh;
+      shell = pkgs.fish;
     };
   };
 
@@ -32,7 +32,7 @@
   programs.fish.enable = true;
 
   # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
