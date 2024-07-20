caf2bb241bfba383f74739cf6763483919788bb4
Shuo Zheng <github@shuo.dev>
2024-07-20 13:50:29 -0400

    feat: configure git/lazygit to use delta

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,21 +4,48 @@
       enable = true;
       userName = "Shuo Zheng";
       userEmail = "github@shuo.dev";
+
       ignores = [
         ".DS_Store"
         ".envrc"
       ];
+
       aliases = {
         root = "rev-parse --show-toplevel";
       };
+
       extraConfig = {
         init.defaultBranch = "main";
         push.autoSetupRemote = true;
         pull.rebase = true;
+        # Following are recommended by delta
+        # https://github.com/dandavison/delta?tab=readme-ov-file#get-started
+        delta.navigate = true;
+        merge.conflictStyle = "diff3";
+        diff.colorMoved = "default";
       };
-      difftastic.enable = true;
+
+      delta = {
+        enable = true;
+        options = {
+          # side-by-side = true;
+        };
+      };
     };
-    lazygit.enable = true;
+
+    lazygit = {
+      enable = true;
+      settings = {
+        git = {
+          paging = {
+            # https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_Pagers.md
+            colorArg = "always";
+            useConfig = false;
+            pager = "delta --dark --paging=never";
+          };
+        };
+      };
+    };
   };
 
   home.shellAliases = {
