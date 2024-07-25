0e978edcf1e97d8b9a60b92e28861aabe637686d
Shuo Zheng <github@shuo.dev>
2024-07-25 11:27:03 -0400

    feat: direnv layout poetry, vivid for LS_COLORS

--- modules/shells/default.nix
+++ modules/shells/default.nix
@@ -11,6 +11,33 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      # This is a function that sets up a poetry environment using `layout poetry` in `.envrc`
+      # See https://github.com/direnv/direnv/wiki/Python#poetry
+      stdlib = ''
+        layout_poetry() {
+            PYPROJECT_TOML="$\{PYPROJECT_TOML:-pyproject.toml\}"
+            if [[ ! -f "$PYPROJECT_TOML" ]]; then
+                log_status "No pyproject.toml found. Executing \`poetry init\` to create a \`$PYPROJECT_TOML\` first."
+                poetry init
+            fi
+
+            if [[ -d ".venv" ]]; then
+                VIRTUAL_ENV="$(pwd)/.venv"
+            else
+                VIRTUAL_ENV=$(poetry env info --path 2>/dev/null ; true)
+            fi
+
+            if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+                log_status "No virtual environment exists. Executing \`poetry install\` to create one."
+                poetry install
+                VIRTUAL_ENV=$(poetry env info --path)
+            fi
+
+            PATH_add "$VIRTUAL_ENV/bin"
+            export POETRY_ACTIVE=1  # or VENV_ACTIVE=1
+            export VIRTUAL_ENV
+        }
+      '';
     };
 
     # Use starship for a universal prompt that's consistent across shells
--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -16,12 +16,20 @@
     zoxide.enable = true;
   };
 
-  home.sessionVariables = {
-    # Use bat for man pages
-    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+  home = {
+    # Install `vivid` to generate colors for `ls` (and `lsd`)
+    # TODO: Remove this once we have a better way to handle colors, maybe via programs.lsd.colors
+    packages = [ pkgs.vivid ];
+    sessionVariables = {
+      # Use bat for man pages, though we prob won't need this since
+      # installed batman
+      MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+      LS_COLORS = "$(vivid generate catppuccin-mocha)";
+    };
+
+    shellAliases = {
+      "cat" = "bat --paging=never";
+    };
   };
 
-  home.shellAliases = {
-    "cat" = "bat --paging=never";
-  };
 }
